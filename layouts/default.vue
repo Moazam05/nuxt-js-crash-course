fada89774a9a448bd5b74ef59538258423aa2655
Moazam05 <salmanmoazam08@gmail.com>
2024-12-10 18:56:31 +0500

    default layout

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <Navbar />
+    <h1>This is the Default Layout</h1>
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
